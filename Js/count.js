850db038c35cd5a22b6afea3d481abb7a3c4ed11
Atoleiyu <gaogao1@hotmail.co.jp>
2024-05-03 02:23:36 +0900

    計算処理をリファクタリング

--- Js/count.js
+++ Js/count.js
@@ -1,16 +1,5 @@
 class StatueCalculator
 {
-  // 現在の才能開花レベル
-  level = Number(document.getElementById("level-select").value);
-  // ピース1個との交換に必要な、女神像の数
-  changeRate = Number(document.getElementById("rate-select").value);
-  // 所持しているピースの数
-  have = Number(document.getElementById("have").value);
-  // 現在のレート = 女神像の交換済み数
-  remaining = Number(document.getElementById("remaining-select").value);
-  // 才能開花に必要なレベル毎の目標ピース数
-  targetPiece = {1:650, 2:600, 3:500, 4:300}
-
   constructor() {
     this.execute();
   }
@@ -27,81 +16,90 @@ class StatueCalculator
    *
    */
   execute() {
-    // 画面からのパラメータを用いて、才能開花に必要なピース数を計算する。
-    const needPieces = this.calcNeedStatue(this.have, this.changeRate, this.remaining, this.targetPiece[this.level]);
-    for(let count = 1; count <= (5 - this.level); count++) {
-      const needPiecesKey = "level" + (this.level + count);
-      this.createSpan(this.level, needPieces[needPiecesKey], count);
-    }
+    // 現在の才能開花レベル
+    const level = Number(document.getElementById("level-select").value);
+    // 画面からのパラメータを用いて、才能開花に必要な女神像の数を計算する。
+    const needStatue = this.calcNeedStatue(level);
 
-    console.log("Level = " + this.level + " targetPiece = " + this.targetPiece[this.level]);
-    console.log("Rate = " + this.changeRate);
-    console.log("Have = " + this.have);
-    console.log("Remaining = " + this.remaining);
+    for(let count = 1; count <= (5 - level); count++) {
+      const needStatueKey = "level" + (level + count);
+      this.createSpan(level, needStatue[needStatueKey], count);
+    }
   }
 
   /**
-   * 才能開花に必要なピース数を計算して返す。
+   * 才能開花に必要な女神像の数を計算して返す。
    * 計算は女神像の交換レートと保有ピースを基に計算する。
    * calcNeedStatue
    *
-   * @param {int} have  保有ピース数
-   * @param {int} changeRate  ピース1個との交換に必要な、女神像の数
-   * @param {int} remaining 現在のレートで女神像とピースを交換可能な残り回数
-   * @param {int} targetPiece 指定されたレベルへの才能開花に必要な目標ピース数
+   * @param {int} level  現在の才能開花レベル
    * @return {array} レベル2~4までの必要な女神像の数
    */
-  calcNeedStatue(have, changeRate, remaining, targetPiece) {
-    let needPieces = {"level2":0, "level3":0, "level4":0, "level5":0};
-    let count = 0;
-    // トータルで必要な女神像の数（S = Statue 銅像）
-    let needS = 0;
-    while (have < targetPiece) {
-      count++;
-      have++;
-      remaining--;
-      needS += changeRate;
-      if (remaining === 0 && changeRate < 5) {
-        remaining = 25;
-        changeRate += 1;
+  calcNeedStatue(level) {
+    // 才能開花に必要なレベル毎の目標ピース数
+    const targetPiece = {1:650, 2:600, 3:500, 4:300};
+
+    // 所持しているピースの数
+    let have = Number(document.getElementById("have").value);
+    // 現在の交換レート（ピース1個との交換に必要な、女神像の数）
+    let exchangeRate = Number(document.getElementById("rate-select").value);
+    // 現在の交換レートで、ピースと女神像の交換可能な残り回数
+    let exchangeRemaining = Number(document.getElementById("remaining-select").value);
+
+    let needStatue = {"level2":0, "level3":0, "level4":0, "level5":0};
+
+    let StatueCount = 0;
+
+    for (have += 1; have <= targetPiece[level]; have++) {
+      StatueCount += exchangeRate;
+      exchangeRemaining--;
+      // 交換可能な残り回数が0回かつ交換レートが5未満の場合、残り回数を25回にリセットして交換レートを1つ上げる。
+      if (exchangeRemaining === 0 && exchangeRate < 5) {
+        exchangeRemaining = 25;
+        exchangeRate += 1;
       }
 
-      if (this.level === 1) {
-        if (have === 50) {
-          needPieces["level2"] = needS;
-        }else if (have === 150) {
-          needPieces["level3"] = needS;
-        }else if (have === 350) {
-          needPieces["level4"] = needS;
-        }else if (have >= 650) {
-          needPieces["level5"] = needS;
-        }
-      }else if (this.level === 2) {
-        if (have === 100) {
-          needPieces["level3"] = needS;
-        }else if (have === 300) {
-          needPieces["level4"] = needS;
-        }else if (have === 600) {
-          needPieces["level5"] = needS;
-        }
-      }else if (this.level === 3) {
-        if (have === 200) {
-          needPieces["level4"] = needS;
-        }else if (have === 500) {
-          needPieces["level5"] = needS;
-        }
-      }else if (this.level === 4) {
-        if (have === 300) {
-          needPieces["level5"] = needS;
-        }
+      switch (level) {
+        case 1:
+          if (have === 50) {
+            needStatue["level2"] = StatueCount;
+          }else if (have === 150) {
+            needStatue["level3"] = StatueCount;
+          }else if (have === 350) {
+            needStatue["level4"] = StatueCount;
+          }else if (have === 650) {
+            needStatue["level5"] = StatueCount;
+          }
+          break;
+        case 2:
+          if (have === 100) {
+            needStatue["level3"] = StatueCount;
+          }else if (have === 300) {
+            needStatue["level4"] = StatueCount;
+          }else if (have === 600) {
+            needStatue["level5"] = StatueCount;
+          }
+          break;
+        case 3:
+          if (have === 200) {
+            needStatue["level4"] = StatueCount;
+          }else if (have === 500) {
+            needStatue["level5"] = StatueCount;
+          }
+          break;
+        case 4:
+          if (have === 300) {
+            needStatue["level5"] = StatueCount;
+          }
+          break;
       }
     }
-    return needPieces;
+    return needStatue;
   }
 
-  createSpan(level, needPiece, count) {
+  createSpan(level, needStatue, count) {
     const targetLevel = level + count;
-    console.log("targetLevel = " + targetLevel + ", needPiece = " + needPiece);
+    console.log("targetLevel = " + targetLevel + ", needStatue = " + needStatue);
     // spanタグを作成
     const creSpan = document.createElement("span");
     creSpan.id = "result-span-" + targetLevel;
@@ -109,7 +107,7 @@ class StatueCalculator
     // H5タグを作成
     const getCreH5 = this.createH5(targetLevel);
     getCreH5.innerText = "★" + targetLevel + "に必要な女神像の個数は ";
-    getCreH5.appendChild(creSpan).innerText = needPiece + "個";
+    getCreH5.appendChild(creSpan).innerText = needStatue + "個";
   }
 
   createH5(targetLevel) {
@@ -124,7 +122,7 @@ class StatueCalculator
 const getCalcBtn = document.getElementById("calc-btn"),
       getResultWrap = document.getElementById("result-div-wrap");
 
-getCalcBtn.onclick = (() => {
+getCalcBtn.onclick = () => {
   const StatueCalculatorClass = new StatueCalculator();
   // ResultWrapの中をリセット
   while (getResultWrap.firstChild) {
@@ -132,4 +130,4 @@ getCalcBtn.onclick = (() => {
   }
   console.clear();
   StatueCalculatorClass.execute();
-})
+}
